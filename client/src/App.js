3e0cbfdca85ddbbe7e63493ad728fefba86daadc
Michiocre <michi2806@gmail.com>
2019-01-04 23:05:39 +0100

    React starting

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+const config = require('./config/config.json');
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            responseToPost: '',
+            response: ''
+        };
+    }
+
+    componentDidMount() {
+        this.callApi()
+            .then(res => this.setState({ response: res }))
+            .catch(err => console.log(err));
+    }
+
+    callApi = async () => {
+        const response = await fetch(config.backend + '/hello');
+        const body = await response.text();
+        console.log(await response);
+        if (response.status !== 200) throw Error(body.message);
+            return body;
+    };
+
+    handleSubmit = async e => {
+        e.preventDefault();
+        const response = await fetch(config.backend + '/user/' + this.state.username, {
+            method: 'GET',
+            header: {
+                'Content-Type': 'application/json'
+            }
+        });
+        const body = await response.text();
+
+        this.setState({responseToPost: body});
+    }
+
     render() {
         return (
             <div className="App">
                 <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <p>
-            Edit <code>src/App.js</code> and save to reload.
-                    </p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-            Learn React
-                    </a>
+                    <h1>{this.state.response}</h1>
+                    <form onSubmit={this.handleSubmit}>
+                        <strong>Username:</strong>
+                        <input
+                            type="text"
+                            value={this.state.username}
+                            onChange={e => this.setState({username: e.target.value})}
+                        />
+                        <button type="submit">Nicer</button>
+                    </form>
+                    <p>{this.state.responseToPost}</p>
                 </header>
             </div>
         );
--- server/src/server.js
+++ server/src/server.js
@@ -4,6 +4,26 @@ const fetch = require('node-fetch');
 const config = require('../config/config.json');
 const math = require('./math.js');
 
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 app.get('/user/:username', async (req, res) => {
 
     let userRequest = await fetch('https://osu.ppy.sh/api/get_user' + '?' +
@@ -26,6 +46,10 @@ app.get('/user/:username', async (req, res) => {
 
     //res.send(math.allTimeAverage(request.top));
     res.send(request.top);
+});
+
+app.get('/hello', (req, res) => {
+    res.send('Yeet');
 });
 
 app.listen(8000, () => {
