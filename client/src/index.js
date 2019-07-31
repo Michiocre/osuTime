eb9638ed42b84aa1ed0c36c2d108f91d956bb35d
Michi2806 <michi2806@gmail.com>
2019-07-31 11:08:03 +0200

    Subdirectory Hosting Progress

--- client/src/index.js
+++ client/src/index.js
@@ -4,16 +4,11 @@ import Login from './pages/Login';
 import Main from './pages/Main';
 import { Route, BrowserRouter as Router } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
-import { createBrowserHistory } from 'history';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-export const history = createBrowserHistory({
-    basename: process.env.PUBLIC_URL
-});
-
 const routing = (
-    <Router>
+    <Router basename={'/osuTime'}> 
         <div>
             <Route exact path="/" component={Login} />
             <Route path="/main/*" component={Main} />
